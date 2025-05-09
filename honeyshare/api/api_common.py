e7d2ffb34acf4c5925a8e30104b775e487eb9f52
Pedro Melgueira <pedromelgueira@gmail.com>
2025-05-09 18:14:35 +0200

    Adds error handling in requests

--- honeyshare/api/api_common.py
+++ honeyshare/api/api_common.py
@@ -7,4 +7,29 @@ class APICommon:
         self.key = key or config.KEY
 
     def get_request(self, path):
-        return make_request(path, self.key)
+        req = make_request(path, self.key)
+
+        if req.status_code == 403:
+            raise ExNotAuthenticated
+        elif req.status_code != 200:
+            raise ExUnknownError
+
+        try:
+            return req.json()
+        except:
+            raise ExCannotParseJSON
+
+
+class ExNotAuthenticated(Exception):
+    def __init__(self):
+        super().__init__("Not authenticated")
+
+
+class ExUnknownError(Exception):
+    def __init__(self):
+        super().__init__("Unknown Error")
+
+
+class ExCannotParseJSON(Exception):
+    def __init__(self):
+        super().__init__("Cannot Parse JSON")
